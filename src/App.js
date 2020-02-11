4017ac7fc3b5ff2b05e48d2e0b87ff5097a64da9
Zachary Ferreira <Pearlybrah@github.com>
2020-02-10 19:12:01 -0800

    added shopping data with sass styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import HomePage from "./HomePage.component";
+import HomePage from "../src/pages/homepage/Homepage.comnponent";
 
 function App() {
   return (
